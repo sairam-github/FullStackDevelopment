76cdbca47b44e3124175515c06dcbd642ef0a221
Sairam Madem <smadem@ptc.com>
2020-01-20 18:40:37 -0500

    Update progress bar in a timer

--- custom_progress_bar/code.js
+++ custom_progress_bar/code.js
@@ -1,10 +1,19 @@
-// const progress = $('.progress-done');
+//const progress = $('.progress-done');
 const progress = document.querySelector('.progress-done');
+let progressPercentage = 0;
 
-setInterval(updateProgress, 500);
+var timer = setInterval(updateProgress, 500);
 
 function updateProgress() {
-    progress.style.width = progress.getAttribute('data-done') + '%';
+    progressPercentage += 10;
+
+    if (progressPercentage > 100) {
+        progress.style.opacity = 0;
+        progressPercentage = 0;
+    }
+
+    progress.style.width = (300 * progressPercentage)/100 + 'px';
     progress.style.opacity = 1;
+    progress.innerHTML = progressPercentage + '%';
 }
 
